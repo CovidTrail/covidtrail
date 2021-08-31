092409cf5695420d5ad2d4ee417c36ce13d6ad00
jake imanaka <jake.imanaka@gmail.com>
2021-08-30 23:09:19 -1000

    change header to functional component and add profile icon menu

--- app/imports/ui/components/header.jsx
+++ app/imports/ui/components/header.jsx
@@ -1,58 +1,76 @@
 import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import Switch from '@material-ui/core/Switch';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormGroup from '@material-ui/core/FormGroup';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
 
-import PropTypes from 'prop-types';
-import { withTracker } from 'meteor/react-meteor-data';
-import { withRouter, NavLink } from 'react-router-dom';
-
-const styles = theme => ({
+const styles = makeStyles((theme) => ({
   root: {
-    flexGrow: 1,
+    flexGrow: 1
   },
   title: {
-    flexGrow: 1,
+    flexGrow: 1
   },
-});
+}));
 
-class Header extends React.Component {
+const Home = (props) => {
+  const classes = styles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
 
-  render() {
-    const { classes } = this.props
-    return (
-        <div className={classes.root}>
-          <AppBar position="static">
-            <Toolbar>
-              <Typography variant="h6" className={classes.title}>
-                CovidTrail
-              </Typography>
-              <Button color="inherit">Login</Button>
-            </Toolbar>
-          </AppBar>
-        </div>
-    );
+  const handleIconMenu = (event) => {
+    setAnchorEl(event.currentTarget);
   }
+  const handleClose = () => {
+    setAnchorEl(null);
+  }
+  return (
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" className={classes.title}>
+              CovidTrail
+            </Typography>
+            <IconButton
+                aria-label="account"
+                aria-controls="menu"
+                are-haspopup="true"
+                onClick={handleIconMenu}
+                color="inherit"
+            >
+              <AccountCircle/>
+            </IconButton>
+            <Menu
+                id="menu"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={open}
+                onClose={handleClose}
+            >
+              <MenuItem onClick={handleClose}>Sign in</MenuItem>
+              <MenuItem onClick={handleClose}>Sign up</MenuItem>
+            </Menu>
+          </Toolbar>
+        </AppBar>
+      </div>
+  );
 }
-
-
-
-/** proptypes for current user (to be used later)*/
-Header.propTypes = {
-  classes: PropTypes.object.isRequired,
-  currentUser: PropTypes.string,
-  currentId: PropTypes.string,
-}
-
-/** Linking Meteor user data to component */
-const HeaderContainer = withTracker(() => ({
-  //currentUser: Meteor.user() ? Meteor.user().username : '',
-  //currentId: Meteor.userId(),
-}))(Header);
 
 // export default Header;
-export default withStyles(styles)(withRouter(HeaderContainer));
+export default Home;
