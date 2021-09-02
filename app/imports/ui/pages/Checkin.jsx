8ffdc6ec5e039ef9709bc47311229a8811cd5ba1
jake imanaka <jake.imanaka@gmail.com>
2021-09-01 17:29:52 -1000

    initial checkin page and routing

---
+++ app/imports/ui/pages/Checkin.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import Checkbox from '@material-ui/core/Button';
+
+
+const Checkin = (props) => {
+
+    return(
+        <div></div>
+    );
+}
+
+export default Checkin;
