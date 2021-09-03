28ed10f5869bebe8cefe5b5793e98bcd6569e62a
jake imanaka <jake.imanaka@gmail.com>
2021-09-02 23:08:30 -1000

    add simpl-schema and remove profile api (uneeded)

--- app/imports/api/Profile.js
+++
@@ -1,3 +0,0 @@
-import { Mongo } from 'meteor/mongo';
-import SimpleSchema from 'simpl-schema';
-import { Tracker } from 'meteor/tracker';
--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,5 +1,3 @@
 import { Meteor } from 'meteor/meteor';
 import { Accounts } from 'meteor/accounts-base';
 import { Roles } from 'meteor/alanning:roles';
-
-// Defines mongo collection
