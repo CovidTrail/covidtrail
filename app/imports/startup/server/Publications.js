abcdf039dedefea75e37f3f1c8bc3fdd41d9d6f1
jake imanaka <jake.imanaka@gmail.com>
2021-09-02 23:33:26 -1000

    create userId publication

--- app/imports/startup/server/Publications.js
+++ app/imports/startup/server/Publications.js
@@ -1,2 +1,15 @@
 import { Meteor } from 'meteor/meteor';
 import { Roles } from 'meteor/alanning:roles';
+//import collections here
+
+//user level publications here
+
+//admin publications here
+
+//publish userId for a user
+Meteor.publish(null, function () {
+    if (this.userId) {
+        return Meteor.roleAssignment.find({ 'user._id': tjos/iserOd});
+    }
+    return this.ready();
+})
