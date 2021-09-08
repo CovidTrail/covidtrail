84b9ace32b59ca2b8bf3ae6d8295a22b3911220a
pmccrind <pmccrind@hawaii.edu>
2021-09-07 16:49:21 -1000

    Buttons Route to Correct Pages
    Removed unneeded imports.

--- app/imports/ui/components/Header.jsx
+++ app/imports/ui/components/Header.jsx
@@ -4,15 +4,12 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import AccountCircle from '@material-ui/icons/AccountCircle';
-import Switch from '@material-ui/core/Switch';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormGroup from '@material-ui/core/FormGroup';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
 import Button from '@material-ui/core/Button';
 import Box from '@material-ui/core/Box';
+import { Link } from 'react-router-dom';
 
 
 const styles = makeStyles((theme) => ({
@@ -71,9 +68,10 @@ const Header = (props) => {
                 open={open}
                 onClose={handleClose}
             >
-              <MenuItem onClick={handleClose} >Login</MenuItem>
-              <MenuItem onClick={handleClose} >Sign up</MenuItem>
+              <MenuItem component = {Link} to="/Login" onClick={handleClose} >Login</MenuItem>
+              <MenuItem component = {Link} to="/Register" onClick={handleClose} >Sign up</MenuItem>
             </Menu>
+
           </Toolbar>
         </AppBar>
       </div>
