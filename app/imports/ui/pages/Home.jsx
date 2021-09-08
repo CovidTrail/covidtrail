02a856fc98332ad9f3667c2ba088de2d4156f949
pmccrind <pmccrind@hawaii.edu>
2021-09-07 15:22:59 -1000

    Implemented makeStyle to Buttons and Typography

--- app/imports/ui/pages/Home.jsx
+++ app/imports/ui/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from 'react-dom';
 import { Meteor } from "meteor/meteor";
 import { Link } from 'react-router-dom';
-import { Button, Typography, Grid, List, ListItem, ListItemText, Container } from '@material-ui/core';
+import { Button, Typography, Grid, List, ListItem, ListItemText, Container, Box } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
 
 /* Create a upper level class to stylize the elements*/
@@ -23,6 +23,7 @@ const useStyles = makeStyles((theme) => ({
     textAlign: 'center',
     /* Does not pass Material UI's color. */
     color: 'primary',
+    marginTop: 10,
   },
 
   typography_secondary: {
@@ -30,6 +31,7 @@ const useStyles = makeStyles((theme) => ({
     textAlign: 'center',
     /* Does not pass Material UI's color. */
     color: 'secondary',
+    marginTop: 10,
   },
 
   grid: {
@@ -37,6 +39,13 @@ const useStyles = makeStyles((theme) => ({
     /*flexDirection: 'row',*/
     justifyContent: 'center',
   },
+
+  box: {
+    marginTop: 15,
+    marginBottom: 15,
+    textAlign: 'center',
+
+  }
 
 }));
 
@@ -50,7 +59,7 @@ const Home = (props) => {
 
   return (
       <Container className={classes.container}>
-        <Grid className={classes.grid}>
+        <Grid container className={classes.grid}>
           <Grid item xs={12} className={classes.grid}>
             <Typography className={classes.typography_primary} variant='h2' color='primary'>
               Stop the Trail with You!
@@ -70,82 +79,86 @@ const Home = (props) => {
 
               Already a member?<br/>
             </Typography>
+            <Box className={classes.box}>
               <Button component={Link} to={"/Login"} className={classes.button} variant='outlined' color='primary'>
                 Sign In
               </Button>
-          </Grid>
-          <Grid item xs={6}>
-            <Typography align='center' variant='h6' color='secondary'>
-
-              Not yet a member? Become one in 3 easy steps.
-            </Typography>
-
-            <List>
-              <ListItem>
-                <ListItemText>
-                  1.) Click Register Here
-                </ListItemText>
-              </ListItem>
-              <ListItem>
-                <ListItemText>
-                  2.) Enter your Personal Information
-                </ListItemText>
-              </ListItem>
-              <ListItem>
-                <ListItemText>
-                  3.) Click Register
-                </ListItemText>
-              </ListItem>
-
-              <ListItem>
-                <ListItemText>
-                  Done!
-                </ListItemText>
-              </ListItem>
-            </List>
-
-            <Typography align='center'>
-              <Button component={Link} to={"/Register"} variant='outlined' color='primary'>
-                Register Here
-              </Button>
-            </Typography>
-
+            </Box>
           </Grid>
 
-          <Grid item xs={6}>
-            <Typography align='center' variant='h6' color='primary'>
-              How to do your Part to Stop the Trail
-              <br/>
-              <br/>
-            </Typography>
 
-            <List>
-              <ListItem>
-                <ListItemText>
-                  1.) Login Everyday
-                </ListItemText>
-              </ListItem>
+          <Grid container>
+            <Grid item xs={6}>
+              <Typography className={classes.typography_secondary} variant='h6' color='secondary'>
+                Not yet a member? Become one in 3 easy steps.
+              </Typography>
 
-              <ListItem>
-                <ListItemText>
-                  2.) Track your Symptoms
-                </ListItemText>
-              </ListItem>
+              <List>
+                <ListItem>
+                  <ListItemText>
+                    1.) Click Register Here
+                  </ListItemText>
+                </ListItem>
+                <ListItem>
+                  <ListItemText>
+                    2.) Enter your Personal Information
+                  </ListItemText>
+                </ListItem>
+                <ListItem>
+                  <ListItemText>
+                    3.) Click Register
+                  </ListItemText>
+                </ListItem>
 
-              <ListItem>
-                <ListItemText>
-                  3.) Stay home when you are feeling Sick
-                </ListItemText>
-              </ListItem>
+                <ListItem>
+                  <ListItemText>
+                    Done!
+                  </ListItemText>
+                </ListItem>
+              </List>
 
-              <ListItem>
-                <ListItemText>
-                  4.) Get Vaccinated if you are able
-                </ListItemText>
-              </ListItem>
-            </List>
+              <Box className={classes.box}>
+                <Button component={Link} to={"/Register"} className={classes.button} variant='outlined' color='primary'>
+                  Register Here
+                </Button>
+              </Box>
+
+            </Grid>
+            <Grid item xs={6}>
+              <Typography className={classes.typography_primary} variant='h6' color='primary'>
+                How to do your Part to Stop the Trail
+              </Typography>
+
+              <List>
+                <ListItem>
+                  <ListItemText>
+                    1.) Login Everyday
+                  </ListItemText>
+                </ListItem>
+
+                <ListItem>
+                  <ListItemText>
+                    2.) Track your Symptoms
+                  </ListItemText>
+                </ListItem>
+
+                <ListItem>
+                  <ListItemText>
+                    3.) Stay home when you are feeling Sick
+                  </ListItemText>
+                </ListItem>
+
+                <ListItem>
+                  <ListItemText>
+                    4.) Get Vaccinated if you are able
+                  </ListItemText>
+                </ListItem>
+              </List>
+
+            </Grid>
 
           </Grid>
+
 
         </Grid>
       </Container>
