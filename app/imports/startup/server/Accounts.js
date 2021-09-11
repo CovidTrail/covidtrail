5a8f1f5fc16963343a617041aab4be783949374e
Tsz Ching Wong <tsz_wong@datahouse.com>
2021-09-10 15:18:00 -1000

    allowed user to register accounts on client

--- app/imports/startup/server/Accounts.js
+++ app/imports/startup/server/Accounts.js
@@ -2,18 +2,18 @@ import { Meteor } from 'meteor/meteor';
 import { Accounts } from 'meteor/accounts-base';
 import { Roles } from 'meteor/alanning:roles';
 
-function createUser(email, password, role) {
-    console.log(`Creating user ${email}`);
-    const userID = Accounts.createUser({
-        username: email,
-        email: email,
-        password: password,
-    });
-    if (role === 'admin') {
-        Roles.createRole(role, { unlessExists: true });
-        Roles.addUsersToRoles(userID, 'admin');
-    }
-}
+// function createUser(email, password, role) {
+//     console.log(`Creating user ${email}`);
+//     const userID = Accounts.createUser({
+//         username: email,
+//         email: email,
+//         password: password,
+//     });
+//     if (role === 'admin') {
+//         Roles.createRole(role, { unlessExists: true });
+//         Roles.addUsersToRoles(userID, 'admin');
+//     }
+// }
 
 //startup with default user accounts (change these after first run)
 if (Meteor.users.find().count() === 0) {
@@ -25,3 +25,4 @@ if (Meteor.users.find().count() === 0) {
     console.log('Cannot initialize the database!  Please invoke meteor with a settings file.');
   }
 }
+
--- app/imports/ui/pages/Register.jsx
+++ app/imports/ui/pages/Register.jsx
@@ -1,6 +1,33 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Input, Button } from "@material-ui/core";
+import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
+import { Roles } from 'meteor/alanning:roles';
+import { useState, useEffect } from "react";
+
+function createUser(firstname, lastname, email, password, role) {
+  console.log(`Creating user ${email}`);
+  var userID = Accounts.createUser({
+      username: email,
+      email: email,
+      password: password,
+      profile: {
+        firstname: firstname,
+        lastname: lastname,
+        role: role
+      }
+  });
+  
+  // if (role == 'admin') {
+  //     Roles.createRole(role, { unlessExists: true });
+  //     Roles.addUsersToRoles(userID, 'admin');
+  // }
+  // else if (role == 'user') {
+  //     Roles.createRole(role, { unlessExists: true });
+  //     Roles.addUsersToRoles(userID, 'user');
+  // }
+}
 
 const useStyles = makeStyles({
     loginText: {
@@ -38,26 +65,44 @@ const useStyles = makeStyles({
 export default Register = () => {
     const classes = useStyles();
 
+    const [firstname, setFirstname] = useState("");
+    const [lastname, setLastname] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        createUser(firstname, lastname, email, password, 'user');
+        
+  };
+
   return (
     <div className={classes.container}>
       <h1 className={classes.loginText}>Register</h1>
+      {error && <span className={classes.error}>{error}</span>}
+      <form onSubmit={handleSubmit}>
       <Input
         className={classes.input}
         id="firstname"
         name="firstname"
         placeholder="First Name"
+        onChange={(e) => setFirstname(e.target.value)}
       ></Input>
       <Input
         className={classes.input}
         id="lastname"
         name="lastname"
         placeholder="Last Name"
+        onChange={(e) => setLastname(e.target.value)}
       ></Input>
       <Input
         className={classes.input}
         id="email"
         name="email"
         placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
       ></Input>
       <Input
         className={classes.input}
@@ -65,11 +110,13 @@ export default Register = () => {
         name="password"
         type="password"
         placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
       ></Input>
-      <Button className={classes.button} type="submit">
+      <Button className={classes.button} type="submit" >
         {" "}
         Sign up
       </Button>
+      </form>
     </div>
   );
 };
