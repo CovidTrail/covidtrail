65d01e95cbd9f91db67fd10e7544d38e1406653e
pmccrind <pmccrind@hawaii.edu>
2021-09-15 17:38:05 -1000

    Added settings.production.json to the gitIgnore
    Moved the settings file to the config folder.

--- app/imports/startup/server/Accounts.js
+++ app/imports/startup/server/Accounts.js
@@ -4,30 +4,26 @@ import { Roles } from 'meteor/alanning:roles';
 
 //create functions in server to be called by client
 Meteor.methods(
-  {
-    //create user/admin account
-    createAccount: function (firstname, lastname, email, password, role) {
-    console.log(`Creating user ${email}`);
-    const userID = Accounts.createUser({
-        username: email,
-        email: email,
-        password: password,
-        profile: {
-          firstname: firstname,
-          lastname: lastname,
-        }
-    }, function(err){
-      if (err){
-        console.log(err);
-      }
-    });
+    {
+      //create user/admin account
+      createAccount: function (firstname, lastname, email, password, role) {
+        console.log(`Creating user ${email}`);
+        const userID = Accounts.createUser({
+          username: email,
+          email: email,
+          password: password,
+          profile: {
+            firstname: firstname,
+            lastname: lastname,
+          }
+        });
         Roles.createRole(role, { unlessExists: true });
         Roles.addUsersToRoles(userID, role);
-  }
-}
+      }
+    }
 );
 
-
+//Not using the settings.development.json file
 //startup with default user accounts (change these after first run)
 /*if (Meteor.users.find().count() === 0) {
   console.log('No accounts found');
