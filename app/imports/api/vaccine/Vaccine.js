02195ed6cbe6ec37d79fda7ab989e5f2f985664f
Jake Imanaka <jake.imanaka@gmail.com>
2021-09-23 01:32:05 -1000

    finish vaccine database

---
+++ app/imports/api/vaccine/Vaccine.js
@@ -0,0 +1,21 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+import { Tracker } from 'meteor/tracker';
+
+const Vaccines = new Mongo.Collection('Vaccines');
+
+const VaccineSchema = new SimpleSchema({
+    userId: String,
+    firstNameame: String,
+    lastName: String,
+    vaccineName: String,
+    lotNum1: String,
+    date1: String,
+    lotNum2: String,
+    date2: String,
+    dateOfBirth: String,
+}, { tracker : Tracker });
+
+Vaccines.attachSchema(VaccineSchema);
+
+export { Vaccines, VaccineSchema };
